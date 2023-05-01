36b67e21533a96f7766d7d5f063ac50f725b90d4
Jaryot <jarram999@gmail.com>
2023-05-01 15:27:02 -0700

    set-up server.js, dependencies, and basic file structure

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const app = express ();
+const routes = require ('./routes');
+const PORT = process.env.PORT || 3001;
+
+app.use (express.json ());
+app.use (express.urlencoded ({extended: true}));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
